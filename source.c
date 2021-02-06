c987daadbe100eb5b312615b0c059a2eb62ebe8a
John Hackstadt <hackstad@hoare7.cs.umsl.edu>
2021-02-06 14:54:26 -0600

    Part 1 done.

--- source.c
+++ source.c
@@ -1,1 +1,31 @@
 //main source file for assignment 1
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+extern char **environ;
+void print_vars(){
+	system("clear");
+	char **ptr;
+	char *key;
+	char tempStr[10000];
+	const char delim[2] = "=";
+	for (ptr = environ; *ptr != NULL; ptr++){
+		strcpy(tempStr, *ptr);
+		key = strtok(tempStr, delim);
+		printf("%s=%s\n", key, getenv(key));
+	}
+}
+void update_env(){
+
+}
+
+void replace_env(){
+
+
+}
+int main(int argc, int *argv[]){
+	print_vars();
+	return 0;
+}	
